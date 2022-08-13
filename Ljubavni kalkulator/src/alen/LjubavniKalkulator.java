d3e53e9e5b094caa8f80fa848729bc2aef917f1b
Alen Boric <boricalen@live.com>
2022-08-13 21:40:14 +0200

    Update LjubavniKalkulator.java

--- Ljubavni kalkulator/src/alen/LjubavniKalkulator.java
+++ Ljubavni kalkulator/src/alen/LjubavniKalkulator.java
@@ -5,41 +5,39 @@ import java.util.Arrays;
 public class LjubavniKalkulator {
 
 	public LjubavniKalkulator() {
-		
-		String ime1=" Marta",ime2="Manuel";
-		
-	    String zbroj=ime1.toLowerCase().trim() + ime2.toLowerCase().trim();
-	    
-	   int[]niz=new int[zbroj.length()]; 
-		
+
+		String ime1 = " Marta", ime2 = "Manuel";
+
+		String zbroj = ime1.toLowerCase().trim() + ime2.toLowerCase().trim();
+
+		int[] niz = new int[zbroj.length()];
+
 		char count;
-		
-		for(int i=0;i < zbroj.length();i++) {
-			count=0;
-			for(int j=0;j<zbroj.length();j++) {
-				if(zbroj.charAt(i)== zbroj.charAt(j)) {
+
+		for (int i = 0; i < zbroj.length(); i++) {
+			count = 0;
+			for (int j = 0; j < zbroj.length(); j++) {
+				if (zbroj.charAt(i) == zbroj.charAt(j)) {
 					count++;
 				}
 			}
-			niz[i]=count;
+			niz[i] = count;
 		}
-		
+
 		System.out.println(Arrays.toString(niz));
 		
 		
-		
+
 	}
+
 	
 	
 	
+
 	public static void main(String[] args) {
-		
+
 		new LjubavniKalkulator();
-		
-		
+
 	}
-	
-	
-	
-	
+
 }
