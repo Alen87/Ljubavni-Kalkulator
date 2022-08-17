062c12eba6aeecbdf7241ba09a2c19bbf66f82bb
Alen Boric <boricalen@live.com>
2022-08-17 12:22:47 +0200

    pocetak

--- Ljubavni kalkulator/src/alen/LjubavniKalkulator.java
+++ Ljubavni kalkulator/src/alen/LjubavniKalkulator.java
@@ -1,46 +1,51 @@
 package alen;
 
-import java.util.Arrays;
-
-import javax.swing.JOptionPane;
-
 public class LjubavniKalkulator {
+	
 
-	public LjubavniKalkulator() {
 
-		String ime1 =JOptionPane.showInputDialog("Unesi  prvo ime");
-		String ime2 =JOptionPane.showInputDialog("Unesi drugo ime");
-
-		String zbroj = ime1.toLowerCase().trim() + ime2.toLowerCase().trim();
-
-		int[] niz = new int[zbroj.length()];
-
-		char count;
-
-		for (int i = 0; i < zbroj.length(); i++) {
-			count = 0;
-			for (int j = 0; j < zbroj.length(); j++) {
-				if (zbroj.charAt(i) == zbroj.charAt(j)) {
-					count++;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String ime1="Marta";
+		String ime2="Manuel";
+		
+		
+		String zbrojImena= ime1.trim().toLowerCase() + ime2.trim().toLowerCase();
+		
+		
+		System.out.println(ime1 + "    " + ime2);
+		int brojac = 0;
+		
+		for(int i=0;i<zbrojImena.length();i++) {
+			brojac=0;
+			for(int j=0;j<zbrojImena.length();j++) {
+				if(zbrojImena.charAt(i)==zbrojImena.charAt(j)) {
+					brojac++;
 				}
 			}
-			niz[i] = count;
+			
+			
+			System.out.print(brojac);
 		}
-
-		System.out.println(Arrays.toString(niz));
 		
 		
+		
 
-	}
-
-	
-	
-	
-
-	public static void main(String[] args) {
-
-		new LjubavniKalkulator();
-
+		
+		
+		
+		
 	}
 
 }
